5ba317be7be05e39d6539b18e26dd939ce62d3fe
hww <450557862@qq.com>
2023-11-23 12:04:44 +0800

    feat: QueueWithPriority

--- Queue.js
+++ Queue.js
@@ -5,7 +5,7 @@ function Queue() {
   }
 
   this.dequeue = function() {
-    if (arr.length === 0) return true;
+    if (arr.length === 0) return false;
     return arr.shift();
   }
 
@@ -25,4 +25,57 @@ function Queue() {
     arr = []
     return true;
   }
+}
+
+// 唯一不同，添加Priority，最小优先队列（priority越小越靠前）
+function QueueWithPriority() {
+  let arr = [];
+  function QueueElement(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+
+  this.enqueue = function (element, priority) {
+    const qe = new QueueElement(element, priority);
+    if (priority === undefined) {
+      return arr.push(qe);
+    }
+    let added = false;
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i]?.priority === undefined || qe.priority < arr[i]?.priority ) {
+        arr.splice(i, 0, qe);
+        return arr.length;
+      }
+    }
+    if (!added) {
+      return arr.push(qe);
+    }
+  }
+
+  this.dequeue = function() {
+    if (arr.length === 0) return false;
+    return arr.shift();
+  }
+
+  this.front = function() {
+    return arr[0];
+  }
+
+  this.size = function () {
+    return arr.length;
+  }
+
+  this.isEmpty = function() {
+    return arr.length === 0;
+  }
+
+  this.clear = function () {
+    arr = []
+    return true;
+  }
+}
+
+export {
+  Queue as default,
+  QueueWithPriority
 }
--- __tests__/QueuePriority.js
+++ __tests__/QueuePriority.js
@@ -1,11 +1,27 @@
-import Queue from "../Queue";
+import  { QueueWithPriority as Queue } from "../Queue";
 
-describe("Queue Test", () => {
+describe("QueueWithPriority Test", () => {
   test("enqueue", () => {
     const queue = new Queue();
-    queue.enqueue(1);
-    queue.enqueue(2);
-    expect(queue.front()).toBe(1);
+    const ez = 'hwwNoPriority';
+    queue.enqueue(ez);
+    expect(queue.front()).toEqual({element: ez, priority: undefined });
+
+    const e1 = 'hww1';
+    queue.enqueue(e1, 1);
+    expect(queue.front()).toEqual({element: e1, priority: 1 });
+
+    const e11 = 'hww11';
+    queue.enqueue(e11, 1);
+    expect(queue.front()).toEqual({element: e1, priority: 1 });
+
+    const e01 = 'hww01';
+    queue.enqueue(e01, 0);
+    expect(queue.front()).toEqual({element: e01, priority: 0 });
+
+    const e100 = 'hww100';
+    queue.enqueue(e100, 100);
+    expect(queue.front()).toEqual({element: e01, priority: 0 });
   })
 
   test("dequeue", () => {
@@ -17,17 +33,17 @@ describe("Queue Test", () => {
     queue.enqueue(3);
     queue.dequeue();
     queue.dequeue();
-    expect(queue.front()).toBe(3);
+    expect(queue.front()).toEqual({element: 3, priority: undefined});
   })
 
   test("front", () => {
     const queue = new Queue();
     queue.enqueue(1);
-    expect(queue.front()).toBe(1);
+    expect(queue.front()).toEqual({element: 1, priority: undefined});
     queue.enqueue(2);
-    expect(queue.front()).toBe(1);
+    expect(queue.front()).toEqual({element: 1, priority: undefined});
     queue.dequeue();
-    expect(queue.front()).toBe(2);
+    expect(queue.front()).toEqual({element: 2, priority: undefined});
   })
 
   test("size", () => {
